5d4967423d7aa52b8eb0c370c98549fab6e07c20
Vladimir <ve.hodest@gmail.com>
2016-12-24 16:24:15 +0300

    Добавлен вывод времени.

--- sudoku/main.cpp
+++ sudoku/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <chrono>
 #include "Data.h"
 
 int main(int argc, char **argv)
 {
    Data test;
-   test.Init(/*
+   test.Init(//*
              "* * *  * 3 7  6 * *"
              "* * *  6 * *  * 9 *"
              "* * 8  * * *  * * 4"
@@ -16,7 +17,7 @@ int main(int argc, char **argv)
              "7 * *  * * *  8 * *"
              "* 1 *  * * 9  * * *"
              "* * 2  5 4 *  * * *"//*/
-             //*
+             /*
              "* * *  1 * *  9 2 *"
              "* 6 1  * * *  * * *"
              "5 * *  * * *  8 * *"
@@ -29,28 +30,33 @@ int main(int argc, char **argv)
              "* * 8  * * *  3 * *"
              "* * *  * * *  * 1 *"//*/
              /*
-             "8 * *  * * *  * * *"
-             "* * 3  6 * *  * * *"
-             "* 7 *  * 9 *  * 2 *"
+             "9 * *  * * 5  * * 1"
+             "* * *  * 4 *  * * 8"
+             "* * *  7 * *  3 2 *"
 
-             "* 5 *  * * 7  * * *"
-             "* * *  * 4 5  7 * *"
-             "* * *  1 * *  * 3 *"
+             "* 9 7  2 6 *  * 5 *"
+             "* * *  * * *  * * *"
+             "* 2 *  * 5 4  7 8 *"
 
-             "* * 1  * * *  * 6 8"
-             "* * 8  5 * *  * 1 *"
-             "* 9 *  * * *  4 * *"//*/
+             "* 4 1  * * 6  * * *"
+             "3 * *  * 1 *  * * *"
+             "6 * *  8 * *  * * 9"//*/
              );
    std::cout << "Before:" << std::endl;
    test.Print(Data::PrintType::Compact);
 
    unsigned __int64 steps;
+   
+   auto start = std::chrono::high_resolution_clock::now();
    bool result = test.Solve(steps);
+   auto end = std::chrono::high_resolution_clock::now();
+   
    std::cout << std::endl << "After:" << std::endl;
    test.Print(Data::PrintType::Compact);
 
    std::cout << "Solved: " << (result ? "yes" : "no") << std::endl;
    std::cout << "Brutforce steps: " << steps << std::endl;
    std::cout << "Valid: " << (test.IsValid() ? "yes" : "NO!") << std::endl;
+   std::cout << "Duration: " << std::chrono::duration<double, std::milli>(end-start).count() << " ms" << std::endl;
    return 0;
 }
